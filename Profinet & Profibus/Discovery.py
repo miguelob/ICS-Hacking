edea109e0b7e3c5cd9cfe684f43aff6b3e09efce
Miguel Oleo <46598884+miguelob@users.noreply.github.com>
2021-11-18 16:20:29 +0100

    Reorganization
    Reorganization of folders and docs

--- Profinet & Profibus/Discovery.py
+++ Profinet & Profibus/Discovery.py
@@ -1,10 +1,11 @@
 from scapy.all import *
 import binascii
 
-#010ecf00000 mac dest
-#6805cabcc38e mac origen (CAMBIAR POR LA DEL EQUIPO) -->	ether 00:e0:4c:68:06:28 
-#810000008892fefe05000000000100010004ffff00000000000000000000000000000000000000000000000000000000 Payload + resto
+#010ecf00000 dest mac address (Do not change)
+#6805cabcc38e Origin mac address (Change to your pc mac address) -->	Example: ether 00:e0:4c:68:06:28 
+#810000008892fefe05000000000100010004ffff00000000000000000000000000000000000000000000000000000000 Payload + Others (Do not change)
 
+# Change org mac address and merge 3 parts into one hex string and replace the one below
 raw_pkt = binascii.unhexlify('010ecf00000000e04c680628810000008892fefe05000000000100010004ffff00000000000000000000000000000000000000000000000000000000')
 pn_dcp = Ether(raw_pkt)
 pn_dcp.show()
--- Profinet & Profibus/FlashLED.py
+++ Profinet & Profibus/FlashLED.py
@@ -1,10 +1,11 @@
 from scapy.all import *
 import binascii
 
-#2863369a2104 mac dest S71500 PLC_15
-#6805cabcc38e mac origen (CAMBIAR POR LA DEL EQUIPO) -->	ether 00:e0:4c:68:06:28 
-#810000008892fefd04000000000400000008050300040000010000000000000000000000000000000000000000000000 Payload + resto
+#2863369a2104 dest mac address S71500 PLC_15 (Use Discovery.py and wireshark to get any profinet device address)
+#6805cabcc38e org mac addres (MUST CHANGE TP YOUR PC MAC ADDRESS) -->	Example: ether 00:e0:4c:68:06:28 
+#810000008892fefd04000000000400000008050300040000010000000000000000000000000000000000000000000000 Payload + Others (Do not change)
 
+# Change org mac address and merge 3 parts into one hex string and replace the one below
 raw_pkt = binascii.unhexlify('2863369a210400e04c680628810000008892fefd04000000000400000008050300040000010000000000000000000000000000000000000000000000')
 pn_dcp = Ether(raw_pkt)
 pn_dcp.show()
