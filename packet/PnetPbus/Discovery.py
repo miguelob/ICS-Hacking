302c48321b51f57fb780ce202c81ff834da7e071
Miguel Oleo <miguel.oleo@alu.comillas.edu>
2022-08-31 10:58:25 -0500

    Package fully implemented

--- packet/PnetPbus/Discovery.py
+++ packet/PnetPbus/Discovery.py
@@ -6,7 +6,12 @@ import binascii
 #810000008892fefe05000000000100010004ffff00000000000000000000000000000000000000000000000000000000 Payload + Others (Do not change)
 
 # Change org mac address and merge 3 parts into one hex string and replace the one below
-raw_pkt = binascii.unhexlify('010ecf00000000e04c680628810000008892fefe05000000000100010004ffff00000000000000000000000000000000000000000000000000000000')
-pn_dcp = Ether(raw_pkt)
-pn_dcp.show()
-sendp(pn_dcp,iface="en8")
+class Discovery(mac):
+    def __init__(self, mac):
+        Hex = '010ecf00000000' + mac + '810000008892fefe05000000000100010004ffff00000000000000000000000000000000000000000000000000000000'
+        raw_pkt = binascii.unhexlify(Hex)
+        pn_dcp = Ether(raw_pkt)
+        pn_dcp.show()
+        sendp(pn_dcp,iface="en8")
+        print("Discovery packet sent")
+        print("Please check wireshark for the response")
